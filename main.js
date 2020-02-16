abbe0e71a4d4879de7e6496e7460d70b0200311f
Logan Gayler <logangayler@gmail.com>
2020-02-16 12:10:20 -0800

    presentationversion

---
+++ main.js
@@ -0,0 +1,83 @@
+$(document).ready(function() {
+  var carouselImages = [
+    {
+      image_url: "./Assets/alcohol-alcoholic-bar-beverage-4784.jpg"
+    },
+    {
+      image_url: "./Assets/assorted-wine-bottles-1283219 (2).jpg"
+    },
+    {
+      image_url: "./Assets/bar-billiards-gambling-game-261043.jpg"
+    },
+    {
+      image_url:
+        "./Assets/depth-of-field-photo-of-two-pilsner-glasses-681847.jpg"
+    },
+    {
+      image_url: "./Assets/alcohol-alcoholic-background-beer-301692.jpg"
+    }
+  ]
+  var htmlStr = ""
+  carouselImages.forEach(function(item, i) {
+    htmlStr += `
+       <div id="image${i}" class="pics">
+          <div><img src="${item.image_url}" /></div>
+           <div class= "prev">&laquo</div>
+           <div class= "next">&raquo</div>
+       </div>
+       `
+  })
+  $(".section5").html(htmlStr)
+  $(".pics:first-child").addClass("current opacity")
+
+  $(".next").on("click", function() {
+    var current = $(".current")
+      .attr("id")
+      .substr(5)
+    var nextIndex = Number(current) + 1
+
+    if (nextIndex === carouselImages.length) {
+      nextIndex = 0
+    }
+
+    $(`#image${current}`).removeClass("current opacity")
+    $(`#image${nextIndex}`).addClass("current opacity")
+  })
+  $(".prev").on("click", function() {
+    var current = $(".current")
+      .attr("id")
+      .substr(5)
+    var prevIndex = Number(current) - 1
+
+    if (prevIndex === -1) {
+      prevIndex = carouselImages.length - 1
+    }
+
+    $(`#image${current}`).removeClass("current opacity")
+    $(`#image${prevIndex}`).addClass("current opacity")
+  })
+})
+$(document).ready(function() {
+  $.get("https://obscure-tundra-54269.herokuapp.com/bar-food", function(data) {
+    var html = data.appetizers.map(function(item) {
+      return `<p>${item.name}</p>
+      <p>${item.price}</p>
+      <p>${item.description}</p>`
+    })
+    document.querySelector(".appetizer").innerHTML = html
+
+    var html = data.entrees.map(function(item) {
+      return `<p>${item.name}</p>
+      <p>${item.price}</p>
+      <p>${item.description}</p>`
+    })
+    document.querySelector(".entrees").innerHTML = html
+
+    var html = data.desserts.map(function(item) {
+      return `<p>${item.name}</p>
+      <p>${item.price}</p>
+      <p>${item.description}</p>`
+    })
+    document.querySelector(".dessert").innerHTML = html
+  })
+})
